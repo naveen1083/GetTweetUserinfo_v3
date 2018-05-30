7942eb28dbc60c63b60396c785bf98424a850a45
naveen1083 <38838499+naveen1083@users.noreply.github.com>
2018-05-30 21:36:16 +0530

    Update toscrape-xpath.py

--- quotesbot/spiders/toscrape-xpath.py
+++ quotesbot/spiders/toscrape-xpath.py
@@ -5,18 +5,18 @@ import scrapy
 class ToScrapeSpiderXPath(scrapy.Spider):
     name = 'toscrape-xpath'
     start_urls = [
-        'http://quotes.toscrape.com/',
+        'https://twitter.com/GeertReyniers',
     ]
 
     def parse(self, response):
-        for quote in response.xpath('//div[@class="quote"]'):
-            yield {
-                'text': quote.xpath('./span[@class="text"]/text()').extract_first(),
-                'author': quote.xpath('.//small[@class="author"]/text()').extract_first(),
-                'tags': quote.xpath('.//div[@class="tags"]/a[@class="tag"]/text()').extract()
-            }
+        User = response.xpath("//div[@class='ProfileHeaderCard']
+        yield {
+            'Name': User.xpath('.//h1[@class="ProfileHeaderCard-name"]/a/text()').extract_first(),
+            'Desc': User.xpath('.//p[@class="ProfileHeaderCard-bio u-dir"]/text()').extract_first(),
 
-        next_page_url = response.xpath('//li[@class="next"]/a/@href').extract_first()
-        if next_page_url is not None:
-            yield scrapy.Request(response.urljoin(next_page_url))
+        }
+
+#         next_page_url = response.xpath('//li[@class="next"]/a/@href').extract_first()
+#         if next_page_url is not None:
+#             yield scrapy.Request(response.urljoin(next_page_url))
 

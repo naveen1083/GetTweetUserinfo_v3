e8f56ff81d40d8a7d59527c62ae478a5c030415c
naveen1083 <38838499+naveen1083@users.noreply.github.com>
2018-06-13 18:40:52 +0530

    Update toscrape-xpath.py

--- quotesbot/spiders/toscrape-xpath.py
+++ quotesbot/spiders/toscrape-xpath.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 import scrapy
 import pkgutil
-# from scrapy.spiders import Rule
-# from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import Rule
+from scrapy.linkextractors import LinkExtractor
 
 
 
